b1bbb8d00ac7014c104bdb12243de00105582e35
jia-shianwang <josh83930@gmail.com>
2021-02-12 13:29:47 +0000

    unit test fix

--- snewpdag/tests/test_shape.py
+++ snewpdag/tests/test_shape.py
@@ -9,8 +9,8 @@ from snewpdag.plugins import Shape
 class TestShape(unittest.TestCase):
 
   def test_update(self):
-    n1 = Shape(500, -0.2, 0.3, 5, -0.04, 0.0005, 160, 4, 0.01, 0, 0)
-    n2 = Shape(500, -0.2, 0.3, 5, -0.04, 0.0005, 160, 4, 0.01, 1, 0.001)
+    n1 = Shape(name = "Node1", 500, -0.2, 0.3, 5, -0.04, 0.0005, 160, 4, 0.01, 0, 0)
+    n2 = Shape(name = "Node2", 500, -0.2, 0.3, 5, -0.04, 0.0005, 160, 4, 0.01, 1, 0.001)
     n1.attach(Node('Node1'))
     n2.attach(Node('Node2'))
 
