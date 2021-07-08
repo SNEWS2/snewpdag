6a82c5178adee19a1515c953331baf1ec549383f
YCMatthewTong <tycmatthew@gmail.com>
2021-07-08 13:04:56 +0800

    DistCalc plugin

---
+++ snewpdag/plugins/DistCalc1.py
@@ -0,0 +1,40 @@
+'''
+This plugin estimate the distance to the supernova from the neutrino data and IMF weighted
+
+assuming that the measured count obeys inverse square law
+
+Constructor arguments: 
+    detector: string, "detector name, ordering" ,
+              one of ["IceCube, NO","IceCube, IO","HK, NO","HK, IO","SK, NO","SK, IO",
+              "DUNE, NO","DUNE, IO","JUNO, NO","JUNO, IO"]
+              
+'''
+
+import logging
+import numpy as np
+from snewpdag.dag import Node
+
+
+class DistCalc1(Node):
+    
+    def _init_(self, detector,**kwargs)
+        self.detector = detector
+        super().__init__(**kwargs)
+    
+    def distcalc1(self,data)
+        #dict of IMF weighted 0-50ms signals and errors
+        #{'detector, ordering': [IMF signal, error, frac error]}
+        IMFsignal = {'IceCube, NO': [9169.96028097276, 1536.248563196319, 0.1675305580531208], \
+               'IceCube, IO': [10773.835720043031, 1619.9659745788326, 0.15036111712425132], \
+               'HK, NO': [916.1876727055476, 152.3796730871074, 0.16631927892799808], \
+               'HK, IO': [963.8751402143208, 140.05882795055814, 0.14530806129040283], \
+               'SK, NO': [133.2636614844433, 22.16431608539741, 0.16631927892799786], \
+               'SK, IO': [140.20002039481017, 20.372193156444798, 0.1453080612904028], \
+               'DUNE, NO': [97.57634394839303, 13.777694691000772, 0.14119912812359142], \
+               'DUNE, IO': [161.09154447956922, 17.119642804057936, 0.10627275850737883], \
+               'JUNO, NO': [128.54796152661447, 20.633251789516653, 0.16051014379753317], \
+               'JUNO, IO': [135.26455501942974, 18.91887993385422, 0.13986576107197238]}
+        #N50 = data[50]
+        N50 = 10**4
+        dist1 = np.sqrt(IMFsignal[self.detector][0]*10.0**2/N50)
+    return dist1
---
+++ snewpdag/plugins/DistCalc2.py
@@ -0,0 +1,49 @@
+'''
+This plugin estimate the distance to the supernova from the neutrino data by constraining the progenitor
+assuming one-to-one correspondence bt f_delta and N50_exp (expected 0-50ms count) (f_delta = m*N50_exp + b)
+
+Constructor arguments: 
+    detector: string, "detector name, ordering" ,
+              one of ["IceCube, NO","IceCube, IO","HK, NO","HK, IO","SK, NO","SK, IO",
+              "DUNE, NO","DUNE, IO","JUNO, NO","JUNO, IO"]
+              
+'''
+
+import logging
+import numpy as np
+from snewpdag.dag import Node
+
+
+class DistCalc2(Node):
+    
+    def _init_(self, detector,**kwargs)
+        self.detector = detector
+        super().__init__(**kwargs)
+    
+    def distcalc2(self,data)
+        #dict of f_delta-N50 fit parameters at 10kpc
+        #{'detector, ordering': [m, b, progenitor model variance]}
+        fit_par = {'IceCube, NO': [0.000182, 0.779, 0.11], \
+               'IceCube, IO': [0.000125, 0.342, 0.0656], \
+               'HK, NO': [0.00152, 0.894, 0.0973], \
+               'HK, IO': [0.00119, 0.439, 0.0529], \
+               'SK, NO': [0.0105, 0.894, 0.0973], \
+               'SK, IO': [0.00815, 0.439, 0.0529], \
+               'DUNE, NO': [0.0158, -0.0304, 0.0641], \
+               'DUNE, IO': [0.00978, -0.706, 0.0411], \
+               'JUNO, NO': [0.0109, 0.746, 0.0909], \
+               'JUNO, IO': [0.0088, 0.319, 0.0515]}
+        
+        #N50 = data[50]
+        
+        #f_delta = (data[150]-data[100])/data[50]
+        f_delta = 2.5
+        m = fit_par[self.detector][0]
+        b = fit_par[self.detector][1]
+        N50_exp = (f_delta-b)/m
+        
+        dist2 = np.sqrt(N50_exp*10**2/N50)
+        
+        
+        
+    return dist2
