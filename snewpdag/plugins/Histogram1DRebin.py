4c552eda65e81428278d0f57cce9104b3f380c51
Cal Hewitt <ch789@cam.ac.uk>
2021-07-30 15:19:29 +0100

    Use overflow bin in case of imperfect division

--- snewpdag/plugins/Histogram1DRebin.py
+++ snewpdag/plugins/Histogram1DRebin.py
@@ -1,12 +1,13 @@
 """
 Plugin to re-bin a 1D histogram
 Only combines integer numbers of bins
+Should listen to a Histogram1D; eg. a Histogram1D renderer can listen to this.
 
 Configuration options:
     factor: number of old bins per new bin; eg. factor=5 would rebin a 100-bin hist to 20 bins
 
 Input data:
-    - Reponds only to 'report'
+    - Reponds only to 'report' (this is the only signal Histogram1D seems to use)
     - Rewrites nbins; bins in the data
 
 """
@@ -24,9 +25,7 @@ class Histogram1DRebin(Node):
 
     def report(self, data):
         old_nbins = data['nbins']
-        if (old_nbins % self.factor) != 0:
-            print("ERROR: factor does not divide nbins")
-            return False
+        old_bin_width = (data['xhigh'] - data['xlow']) / old_nbins
 
         new_nbins = old_nbins // self.factor
         new_bins = np.empty(new_nbins, 'float64')
@@ -37,16 +36,15 @@ class Histogram1DRebin(Node):
             new_bin_value = np.sum(data['bins'][i_new_bin * self.factor : (i_new_bin + 1) * self.factor])
             new_bins[i_new_bin] = new_bin_value
 
+        # Add everything else to the overflow bin and change the top limit...
+        data['overflow'] += np.sum(data['bins'][new_nbins * self.factor:])
+        data['xhigh'] = data['xlow'] + old_bin_width * (old_nbins // self.factor) * self.factor
+
+        # Update n_bins and bins in the payload
         data['nbins'] = new_nbins
         data['bins'] = new_bins
 
         return True
 
-"""
-
-Qs:
-    - When should I be returning false/true?
-    - Is it alright just to OVERWRITE data like that?
-
-
-"""
+    def alert(self, data):
+        return self.report(data)
