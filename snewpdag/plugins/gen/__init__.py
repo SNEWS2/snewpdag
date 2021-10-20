81849ddd1244f648b3cb20358e4ec7712a532d18
Riccardo Dallavalle <riccardo.dallavalle.001@student.uni.lu>
2021-10-20 14:19:50 +0100

    Imported new plugins

--- snewpdag/plugins/gen/__init__.py
+++ snewpdag/plugins/gen/__init__.py
@@ -1,7 +1,11 @@
 
 from .TimeDistSource import TimeDistSource
+from .DeltatCalculator import DeltatCalculator
 from .TimeDist import TimeDist
 from .TimeSeries import TimeSeries
+from .DetectorTime import DetectorTime
 from .GenerateSGBG import GenerateSGBG
+from .NeutrinoArrivalTime import NeutrinoArrivalTime
+from .TimeOffset import TimeOffset
 
 from .Combine import Combine
