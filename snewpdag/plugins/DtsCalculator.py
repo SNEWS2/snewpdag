bdb966d06bc24f067a406716f0a0a445f988bfb7
Riccardo Dallavalle <riccardo.dallavalle.001@student.uni.lu>
2021-12-03 12:09:45 +0000

    Added comments and cleaned the code

--- snewpdag/plugins/DtsCalculator.py
+++ snewpdag/plugins/DtsCalculator.py
@@ -1,19 +1,26 @@
 """
-DeltatCalculator: compute the time difference between the observed t0s
-                    from the data[neutrino_time] field of two detectors.
+DtsCalculator: compute neutrino time dts between each detector sending an alert.
 
-Output:
-    Time difference between the respective t0s, (s,ns)
+Input payload:
+    data[neutrino_time] = observed burst time for a detector (s,ns). For mc trials use
+    the Detector.py plugin to generate this.
+
+Output payload:
+    dts: a dictionary of time differences. Keys are of form (det1, det2). The values are themselves dictionaries with
+    the following keys:
+    dt: time tuple (s,ns) of t1-t2
+    t1: neutrino time tuple (s,ns) of det1
+    t2: neutrino time tuple (s,ns) of det2
 """
 
 import logging
 import numpy as np
 from snewpdag.dag import Node, lib
 
-class DeltaTCalculator(Node):
+class DtsCalculator(Node):
         def __init__(self, **kwargs):
             self.valid = [False] * 10  # flags indicating valid data from sources
-            self.t = [None] * 10  # observed first nu event time for each detector
+            self.t = [None] * 10  # neutrino time for each detector
             self.h = [0.0] * 10  # histories from each source
             self.dets_names = [None] * 10
             self.dets_nu_times = [0.0] * 10
@@ -23,24 +30,26 @@ class DeltaTCalculator(Node):
             super().__init__(**kwargs)
 
         def alert(self, data):
+           # verify whether there are enough input nodes to do a calculation:
             index = self.last_watch_index()
             if index < 0:
                 source = self.last_source
                 logging.error("[{}] Unrecognized source {}".format(self.name, source))
                 return False
 
-            time = self.t[index]
+           # extract neutrino time for each detector:
+            burst_time = self.t[index]
             self.t[index] = data.get('neutrino_time')
-            for det_name, value in data['gen']['neutrino_times'].items():
-                if value == self.t[index]:
+            for det_name, nu_time in data['gen']['neutrino_times'].items():
+                if nu_time == self.t[index]:
                     #det = self.db.get(det_name)
                     #det_uncertainty = det.sigma
                     #self.uncertainties[index] = det_uncertainty
                     self.dets_names[index] = det_name
-                    self.dets_nu_times[index] = value
+                    self.dets_nu_times[index] = nu_time
 
-            if self.t[index] != time:
-                # verify whether experiment is revoking data
+           # verify whether an experiment is revoking its data:
+            if self.t[index] != burst_time:
                 if self.t[index] == None:
                     if self.valid[index]:
                         self.valid[index] = False
@@ -49,18 +58,19 @@ class DeltaTCalculator(Node):
                 else:
                     self.valid[index] = True
 
-                # compute the dts if we have two or more valid inputs
+               # compute the dts if we have two or more valid inputs
                 if sum(self.valid) >= 2:
-                    DeltaTMatrix = [[0 for j in range(sum(self.valid))] for i in range(sum(self.valid))]
+                   # store dts in a 2d list:
+                    DeltaTsMatrix = [[0 for j in range(sum(self.valid))] for i in range(sum(self.valid))]
                     for i in range(sum(self.valid)):
                         for j in range(i+1, sum(self.valid)):
-                            DeltaTMatrix[i][j] = np.subtract(self.t[i], self.t[j])
-
+                            DeltaTsMatrix[i][j] = np.subtract(self.t[i], self.t[j])
+                           # add to the payload:
                             if 'dts' not in data:
-                                data['dts'] = {(self.dets_names[i], self.dets_names[j]): {'dt': tuple(lib.normalize_time_difference(DeltaTMatrix[i][j])),\
+                                data['dts'] = {(self.dets_names[i], self.dets_names[j]): {'dt': tuple(lib.normalize_time_difference(DeltaTsMatrix[i][j])),\
                                                                 't1': self.dets_nu_times[i], 't2': self.dets_nu_times[j]}}
                             else:
-                                data['dts'].update({(self.dets_names[i], self.dets_names[j]): {'dt': tuple(lib.normalize_time_difference(DeltaTMatrix[i][j])), \
+                                data['dts'].update({(self.dets_names[i], self.dets_names[j]): {'dt': tuple(lib.normalize_time_difference(DeltaTsMatrix[i][j])), \
                                                                      't1': self.dets_nu_times[i], 't2': self.dets_nu_times[j]}})
 
                    # data['dt_uncertainties'] = {"Diff_t0s": max(self.uncertainties)}
@@ -70,12 +80,10 @@ class DeltaTCalculator(Node):
                    # data['history'].combine((self.h[0], self.h[1]))
                    # in fact, this should even work if we return True,
                    # since the payload has been updated in place.
-                    logging.warning(data)
                     return data
-
-                # not enough inputs
+               # not enough inputs
                 return False
-            # no update
+           # no update
             return False
 
         def revoke(self, data):
