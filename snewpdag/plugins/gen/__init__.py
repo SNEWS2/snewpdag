b29d65c5da4eff1f872558467e946d93cedbfcf2
mcolomermolla <mcolomermolla@km3net.de>
2022-02-10 20:08:35 +0100

    Add new generators in the __init__ script

--- snewpdag/plugins/gen/__init__.py
+++ snewpdag/plugins/gen/__init__.py
@@ -3,6 +3,8 @@ from .TimeDistSource import TimeDistSource
 from .TimeDist import TimeDist
 from .TimeSeries import TimeSeries
 from .GenerateSGBG import GenerateSGBG
+from .Generate_bg_glitch import Generate_bg_glitch
+from .Generate_delta_peak import Generate_delta_peak
 
 from .Combine import Combine
 
