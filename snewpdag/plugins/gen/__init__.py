a3a9a0f2088781eafe7301aea17c5a765ce62727
Riccardo D <42295487+dallaval5u@users.noreply.github.com>
2022-03-21 15:28:41 +0100

    Update __init__.py

--- snewpdag/plugins/gen/__init__.py
+++ snewpdag/plugins/gen/__init__.py
@@ -7,6 +7,13 @@ from .Generate_bg_glitch import Generate_bg_glitch
 from .Generate_delta_peak import Generate_delta_peak
 from .GenerateSGBG_deadtimes import GenerateSGBG_deadtimes
 
+####validators
+from .DUNE_validator import DUNE_validator
+from .DS20K_validator import DS20K_validator
+from .JUNO_validator import JUNO_validator
+from .Baksan_validator import Baksan_validator
+from .Xenon_validator import Xenon_validator
+
 from .Combine import Combine
 
 from .TrueDist import TrueDist
