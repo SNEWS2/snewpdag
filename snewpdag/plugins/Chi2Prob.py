d83a747e66db25682eaf9aa8642e2792459373d9
Jeff Tseng <jeff.tseng@physics.ox.ac.uk>
2022-04-13 14:53:50 +0100

    add comments about pixel center approximation

--- snewpdag/plugins/Chi2Prob.py
+++ snewpdag/plugins/Chi2Prob.py
@@ -5,7 +5,16 @@ arguments:
   in_field: field name of chi2 values
   out_field: field name of probability values
 
-The probability map is normalized to unit area
+Note that chi2 is calculated at the center of each pixel (in DiffPointing),
+so the calculation here starts by calculating the probability density at
+each pixel center.  We take this central pdf value as proportional to
+the average probability over the pixel.  Since all healpix pixels have
+equal area, we sum all these pdf values and then divide by the sum.
+The result should be a probability map normalized to unit area,
+within the central-value-as-average approximation.
+
+Confidence levels calculated with the resulting skymap won't be quite the
+same as those calculated with Chi2CL, given the approximations in both.
 """
 import logging
 import numpy as np
