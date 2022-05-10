835fc6f4d03be05279e9108a117bc477b102fabc
Jeff Tseng <jeff.tseng@physics.ox.ac.uk>
2022-05-10 10:02:47 +0100

    multiple simulation test

---
+++ snewpdag/data/test-mult-dt.py
@@ -0,0 +1,17 @@
+{ 'name': 'Gen0', 'action': 'alert', 'burst_id':0, 'trial_id': 0 }
+{ 'name': 'Gen0', 'action': 'reset', 'burst_id':0, 'trial_id': 0 }
+{ 'name': 'Gen1', 'action': 'alert', 'burst_id':0, 'trial_id': 1 }
+{ 'name': 'Gen1', 'action': 'reset', 'burst_id':0, 'trial_id': 1 }
+{ 'name': 'Gen2', 'action': 'alert', 'burst_id':0, 'trial_id': 2 }
+{ 'name': 'Gen2', 'action': 'reset', 'burst_id':0, 'trial_id': 2 }
+{ 'name': 'Gen3', 'action': 'alert', 'burst_id':0, 'trial_id': 3 }
+{ 'name': 'Gen3', 'action': 'reset', 'burst_id':0, 'trial_id': 3 }
+{ 'name': 'Gen4', 'action': 'alert', 'burst_id':0, 'trial_id': 4 }
+{ 'name': 'Gen4', 'action': 'reset', 'burst_id':0, 'trial_id': 4 }
+{ 'name': 'Gen5', 'action': 'alert', 'burst_id':0, 'trial_id': 5 }
+{ 'name': 'Gen5', 'action': 'reset', 'burst_id':0, 'trial_id': 5 }
+{ 'name': 'Gen6', 'action': 'alert', 'burst_id':0, 'trial_id': 6 }
+{ 'name': 'Gen6', 'action': 'reset', 'burst_id':0, 'trial_id': 6 }
+{ 'name': 'Gen7', 'action': 'alert', 'burst_id':0, 'trial_id': 7 }
+{ 'name': 'Gen7', 'action': 'reset', 'burst_id':0, 'trial_id': 7 }
+{ 'name': 'Gen0', 'action': 'report', 'burst_id':0 }
