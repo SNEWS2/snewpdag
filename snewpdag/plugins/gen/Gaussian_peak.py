dc9fbb2d96804e20639f365c399142a9593c8131
HermanEiTse <109292214+HermanEiTse@users.noreply.github.com>
2022-08-06 04:22:49 +0100

    Update Gaussian_peak.py
    The naming is improved.
    Data generated outside the range is ignored now.

--- snewpdag/plugins/gen/Gaussian_peak.py
+++ snewpdag/plugins/gen/Gaussian_peak.py
@@ -4,8 +4,8 @@ Gaussian_peak - generate a gaussian_peak to add to a TimeSeries or TimeHist
 Arguments:
   field:  name of field. Must be TimeSeries or TimeHist. Modified in place.
   event:  number of events to be added.
-  scale:  Standard deviation (spread or “width”) of the distribution.
-  expv:   Mean (“centre”)(seconds) of the distribution.
+  stdev:  Standard deviation (spread or “width”) of the distribution.
+  expv:   Expected value (“centre”)(seconds) of the distribution.
 """
 import numpy as np
 from snewpdag.dag import Node
@@ -13,11 +13,12 @@ from snewpdag.dag.lib import ns_per_second
 from snewpdag.values import TimeSeries, TimeHist
 
 class Gaussian_peak (Node):
-  def __init__(self, field, event, scale=1, expv=2, **kwargs):
+  def __init__(self, field, event, stdev=1, expv=2, **kwargs):
     self.field = field  
     self.event = event
-    self.scale = scale
+    self.stdev = stdev
     self.expv = expv
+    self.duration = kwargs.pop('duration', 10.0)
     super().__init__(**kwargs)
 
   def alert(self, data):
@@ -26,8 +27,9 @@ class Gaussian_peak (Node):
       if not isinstance(v, TimeHist) and not isinstance(v, TimeSeries):
         return False # v is neither TimeHist nor TimeSeries
       nev = Node.rng.poisson(self.event) # Poisson fluctuations
-      u = Node.rng.normal(loc=self.expv * ns_per_second, scale=self.scale * ns_per_second, size=nev)
-      v.add_offsets(u)
+      u = Node.rng.normal(loc=self.expv * ns_per_second, scale=self.stdev * ns_per_second, size=nev)
+      mu = np.ma.masked_array(u, mask=((u<0.0)|(u>self.duration)))
+      v.add_offsets(mu)
       return True
     else:
       return False
