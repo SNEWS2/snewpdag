ad19d595530e0b9d8d5dcdb3389b81d2d89b9235
Jeff Tseng <j.tseng1@physics.ox.ac.uk>
2023-03-17 14:36:11 +0000

    take ndof from payload

--- snewpdag/plugins/Chi2Prob.py
+++ snewpdag/plugins/Chi2Prob.py
@@ -3,6 +3,7 @@ Chi2Prob - take a chi2 map and turn it into probability map
 
 arguments:
   in_field: field name of chi2 values
+  in_ndof_field: field name of ndof value
   out_field: field name of probability values
 
 Note that chi2 is calculated at the center of each pixel (in DiffPointing),
@@ -23,19 +24,21 @@ from scipy.stats import chi2
 from snewpdag.dag import Node
 
 class Chi2Prob(Node):
-  def __init__(self, in_field, out_field, **kwargs):
+  def __init__(self, in_field, in_ndof_field, out_field, **kwargs):
     self.in_field = in_field
+    self.in_ndof_field = in_ndof_field
     self.out_field = out_field
     super().__init__(**kwargs)
 
   def alert(self, data):
-    if self.in_field in data:
-      m = np.array(data[self.in_field])
-      base = np.min(m)
-      v = m - base
-      c = chi2.pdf(v, df=2)
+    if self.in_field in data and self.in_ndof_field in data:
+      #m = np.array(data[self.in_field])
+      #base = np.min(m)
+      #v = m - base
+      v = np.array(data[self.in_field])
+      c = chi2.pdf(v, df=data[self.in_ndof_field])
       sc = np.sum(c)
-      logging.info('Sum of probability map is {}'.format(sc))
+      logging.info('Sum of probability map is {}, chi2 range ({}, {}), ndof = {}'.format(sc, np.min(v), np.max(v), data[self.in_ndof_field]))
       data[self.out_field] = c / sc
       return data
     else:
